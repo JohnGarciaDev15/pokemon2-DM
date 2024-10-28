e96d3617fad4763f5395835655c742627a1b5331
JohnGarciaDev15 <johngarciadev15@gmail.com>
2024-10-28 18:30:23 -0500

    actualización de archivo DM. Se cambia la linea número 9

--- src/Pokemon2Dm.js
+++ src/Pokemon2Dm.js
@@ -8,12 +8,15 @@ export class Pokemon2Dm extends LitElement {
 
     const pokemonDetails = await Promise.all(
       data.results.map(pokemon => fetch(pokemon.url).then(res => res.json()))
-    )
+    );
  
-    this.pokemons = pokemonDetails.filter(pokemon => !pokemon.evolves_from_species).map(pokemon => ({
+    const filteredPokemons = pokemonDetails.filter(pokemon => !pokemon.evolves_from_species).map(pokemon => ({
       name: pokemon.name,
       image: pokemon.sprites.front_default,
       abilities: pokemon.abilities.map(ability => ability.ability.name).join(', '),
     }));
+
+    console.log('Filtered Pokemon:', filteredPokemons);
+    return filteredPokemons;
   }
 }
